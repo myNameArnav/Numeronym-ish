efd84daf350f6044f45f454b3eaa04cf4e6f4366
Arnav <arnjain3@gmail.com>
2019-08-25 00:35:42 +0530

    nothing much.

--- findNreplace.py
+++ findNreplace.py
@@ -1,5 +1,12 @@
 sentenceSplit = ['hate', 'in', 'the', 'nation']
 pspelling = ['heɪt', 'tə', 'ðə', 'ˈneɪʃ(ə)n']
 numPspelling = ['wʌn', 'tuː', 'θriː', 'fɔː', 'fʌɪv', 'sɪks', 'ˈsɛv(ə)n', 'eɪt', 'nʌɪn', 'ˈzɪərəʊ']
-
+numspelling = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
 #check if pspelling has any of numPspelling in them
+combined = dict(zip(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero'], ['wʌn', 'tuː', 'θriː', 'fɔː', 'fʌɪv', 'sɪks', 'ˈsɛv(ə)n', 'eɪt', 'nʌɪn', 'ˈzɪərəʊ']))
+s = " "
+s = s.join(pspelling)
+for i in range(0, len(numPspelling)):
+    find = s.find(numPspelling[i])
+    i = i + 1
+print(combined)
