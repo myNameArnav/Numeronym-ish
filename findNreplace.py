ceb711a094951d520c8b843c2f1b99e07ba42b75
Arnav <arnjain3@gmail.com>
2019-08-25 00:07:59 +0530

    added findNreplace

---
+++ findNreplace.py
@@ -0,0 +1,5 @@
+sentenceSplit = ['hate', 'in', 'the', 'nation']
+pspelling = ['heɪt', 'tə', 'ðə', 'ˈneɪʃ(ə)n']
+numPspelling = ['wʌn', 'tuː', 'θriː', 'fɔː', 'fʌɪv', 'sɪks', 'ˈsɛv(ə)n', 'eɪt', 'nʌɪn', 'ˈzɪərəʊ']
+
+#check if pspelling has any of numPspelling in them
