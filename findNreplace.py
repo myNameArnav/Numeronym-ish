46c9852a4b8601d657733e7c7b4c3207fa2b7a96
Arnav <arnjain3@gmail.com>
2019-08-25 01:29:54 +0530

    Optimised

--- findNreplace.py
+++ findNreplace.py
@@ -7,5 +7,8 @@ num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 #check if pspelling has any of numPspelling in them
 
 combined = {'one': 'wʌn', 'two': 'tuː', 'three': 'θriː', 'four': 'fɔː', 'five': 'fʌɪv', 'six': 'sɪks', 'seven': 'ˈsɛv(ə)n', 'eight': 'eɪt', 'nine': 'nʌɪn', 'zero': 'ˈzɪərəʊ'}
+example = sentenceSplit[0]
+print(example)
 
+for i in range(len(example)):
     
