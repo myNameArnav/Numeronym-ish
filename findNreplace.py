1c3c0ce5b4bedaea528eddedf0ed2a7a4d7c2509
Arnav <arnjain3@gmail.com>
2019-08-25 00:36:14 +0530

    nothing much

--- findNreplace.py
+++ findNreplace.py
@@ -2,11 +2,10 @@ sentenceSplit = ['hate', 'in', 'the', 'nation']
 pspelling = ['heɪt', 'tə', 'ðə', 'ˈneɪʃ(ə)n']
 numPspelling = ['wʌn', 'tuː', 'θriː', 'fɔː', 'fʌɪv', 'sɪks', 'ˈsɛv(ə)n', 'eɪt', 'nʌɪn', 'ˈzɪərəʊ']
 numspelling = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
 #check if pspelling has any of numPspelling in them
-combined = dict(zip(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero'], ['wʌn', 'tuː', 'θriː', 'fɔː', 'fʌɪv', 'sɪks', 'ˈsɛv(ə)n', 'eɪt', 'nʌɪn', 'ˈzɪərəʊ']))
-s = " "
-s = s.join(pspelling)
-for i in range(0, len(numPspelling)):
-    find = s.find(numPspelling[i])
-    i = i + 1
-print(combined)
+
+combined = {'one': 'wʌn', 'two': 'tuː', 'three': 'θriː', 'four': 'fɔː', 'five': 'fʌɪv', 'six': 'sɪks', 'seven': 'ˈsɛv(ə)n', 'eight': 'eɪt', 'nine': 'nʌɪn', 'zero': 'ˈzɪərəʊ'}
+
+    
