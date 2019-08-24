aedbfb32e1c8a4f5da21f91bf0dfea0a1551d6c6
Arnav <arnjain3@gmail.com>
2019-08-24 22:46:03 +0530

    webscraper works

--- webscraper.py
+++ webscraper.py
@@ -2,14 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 sentenceSplit = ['hate', 'in', 'the', 'nation']
-firstelement = str(sentenceSplit[0])
-lastelement = str(sentenceSplit[-1])
-"""for i in range(firstelement, lastelement):i = i + 1"""
-url = "https://www.lexico.com/en/definition/" + "one" + "/"
-print(url)
-r = requests.get(url)
-html_content = r.text
-soup = BeautifulSoup(html_content, "html.parser")
-line = soup.find(class_ = "phoneticspelling").text
-spelling = line.replace("/", "")
-print(spelling)
+elements = int(len(sentenceSplit))
+pspelling = []
+for i in range(-1, elements-1):
+    i = i + 1
+    url = "https://www.lexico.com/en/definition/" + str(sentenceSplit[i]) + "/"
+    print(url)
+    r = requests.get(url)
+    html_content = r.text
+    soup = BeautifulSoup(html_content, "html.parser")
+    line = soup.find(class_ = "phoneticspelling").text
+    spelling = line.replace("/", "")
+    pspelling.append(spelling)
+print(pspelling)
