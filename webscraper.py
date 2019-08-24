d15d23054d7fc5029601896c9ce913690e0ffbed
Arnav <arnjain3@gmail.com>
2019-08-25 01:30:35 +0530

    Optimised Again.

--- webscraper.py
+++ webscraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 sentenceSplit = ['hate', 'in', 'the', 'nation']
 elements = len(sentenceSplit)
 pspelling = []
+
 for i in range(0, elements):
     word = str(sentenceSplit[i])
     url = "https://www.lexico.com/en/definition/" + word + "/"
@@ -11,8 +12,9 @@ for i in range(0, elements):
     r = requests.get(url)
     html_content = r.text
     soup = BeautifulSoup(html_content, "html.parser")
-    line = soup.find(class_ = "phoneticspelling").text
+    line = soup.find(class_="phoneticspelling").text
     spelling = line.replace("/", "")
     pspelling.append(spelling)
     i = i + 1
+
 print(pspelling)
