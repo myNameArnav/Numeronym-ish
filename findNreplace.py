25c25b2d2d4e2893570681b0ea3118a5f26eeb8f
Arnav <arnjain3@gmail.com>
2019-08-25 21:09:26 +0530

    still not working

--- findNreplace.py
+++ findNreplace.py
@@ -1,9 +1,6 @@
-from flashtext import KeywordProcessor
-keyword_processor = KeywordProcessor()
-
 sentence = 'hate in the nation'
 sentenceSplit = ['hate', 'in', 'the', 'nation']
-pspelling = ['heɪt', 'tə', 'ðə', 'ˈneɪʃ(ə)n']
+pspelling = ['ðɛː', 'ɪz', 'ə', 'lɒt', 'ɒv', 'heɪt', 'ɪn', 'ðə', 'ˈneɪʃ(ə)n', 'wɪð', 'mɔː', 'heɪt']
 numPspelling = ['wʌn', 'tuː', 'θriː', 'fɔː', 'fʌɪv', 'sɪks', 'ˈsɛv(ə)n', 'eɪt', 'nʌɪn', 'ˈzɪərəʊ']
 #numspelling = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
@@ -14,8 +11,12 @@ combined = {'1': 'wʌn', '2': 'tuː', '3': 'θriː', '4': 'fɔː', '5': 'fʌɪv'
 print(sentenceSplit)
 
 for j in range(0, len(numPspelling)):
-    for i in range(0, len(sentenceSplit)):
+    for i in range(0, len(pspelling)):
         subfinder = pspelling[i].find(numPspelling[j])
-        print(subfinder)
+        if subfinder != -1:
+            print()
+        else:
+            print()
+        print(subfinder,pspelling[i],numPspelling[j])
         i = i + 1
     j = j + 1
