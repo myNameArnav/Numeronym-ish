aa09db8dc34d73976503d85c93d90dd6e2c4cb42
Arnav <arnjain3@gmail.com>
2019-08-25 18:15:50 +0530

    Substring works, have to figure out find&replace

--- findNreplace.py
+++ findNreplace.py
@@ -1,10 +1,11 @@
 from flashtext import KeywordProcessor
 keyword_processor = KeywordProcessor()
 
+sentence = 'hate in the nation'
 sentenceSplit = ['hate', 'in', 'the', 'nation']
 pspelling = ['heɪt', 'tə', 'ðə', 'ˈneɪʃ(ə)n']
 numPspelling = ['wʌn', 'tuː', 'θriː', 'fɔː', 'fʌɪv', 'sɪks', 'ˈsɛv(ə)n', 'eɪt', 'nʌɪn', 'ˈzɪərəʊ']
-numspelling = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
+#numspelling = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 sentence1 = 'hate in the nation'
 #check if pspelling has any of numPspelling in them
@@ -12,10 +13,9 @@ sentence1 = 'hate in the nation'
 combined = {'1': 'wʌn', '2': 'tuː', '3': 'θriː', '4': 'fɔː', '5': 'fʌɪv', '6': 'sɪks', '7': 'ˈsɛv(ə)n', '8': 'eɪt', '9': 'nʌɪn', '0': 'ˈzɪərəʊ'}
 print(sentenceSplit)
 
-for i in range(0,len(sentenceSplit)):
-    findingword = sentenceSplit[i]
-    for j in range(0, len(numPspelling)):
-        findingnum = numPspelling[j]
-        #Main Logic
-        j = j + 1
-    i = i + 1
+for j in range(0, len(numPspelling)):
+    for i in range(0, len(sentenceSplit)):
+        subfinder = pspelling[i].find(numPspelling[j])
+        print(subfinder)
+        i = i + 1
+    j = j + 1
