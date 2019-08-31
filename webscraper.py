1cb097b4f57439d9f08134b3c7733ba9642358e9
Arnav <arnjain3@gmail.com>
2019-08-31 13:37:32 +0530

    Formating - Spaces to Tabs

--- webscraper.py
+++ webscraper.py
@@ -6,15 +6,15 @@ elements = len(sentenceSplit)
 pspelling = []
 
 for i in range(0, elements):
-    word = str(sentenceSplit[i])
-    url = "https://www.lexico.com/en/definition/" + word + "/"
-    print(url)
-    r = requests.get(url)
-    html_content = r.text
-    soup = BeautifulSoup(html_content, "html.parser")
-    line = soup.find(class_="phoneticspelling").text
-    spelling = line.replace("/", "")
-    pspelling.append(spelling)
-    i = i + 1
+	word = str(sentenceSplit[i])
+	url = "https://www.lexico.com/en/definition/" + word + "/"
+	print(url)
+	r = requests.get(url)
+	html_content = r.text
+	soup = BeautifulSoup(html_content, "html.parser")
+	line = soup.find(class_="phoneticspelling").text
+	spelling = line.replace("/", "")
+	pspelling.append(spelling)
+	i = i + 1
 
 print(pspelling)
