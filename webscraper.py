d9568517cb38bfe803400f2d18ecb72ef63f2bc6
Arnav <arnjain3@gmail.com>
2019-10-31 07:25:01 +0530

    minor tweeks

--- webscraper.py
+++ webscraper.py
@@ -8,7 +8,7 @@ pspelling = []
 for i in range(0, elements):
 	word = str(sentenceSplit[i])
 	url = "https://www.lexico.com/en/definition/" + word + "/"
-	print(url)
+	#print(url)
 	r = requests.get(url)
 	html_content = r.text
 	soup = BeautifulSoup(html_content, "html.parser")
@@ -16,5 +16,8 @@ for i in range(0, elements):
 	spelling = line.replace("/", "")
 	pspelling.append(spelling)
 	i = i + 1
+	print(str(i) + "th word is " + str(sentenceSplit[i]))
+	print(pspelling)
 
-print(pspelling)
+
+#['ə', 'əˈbɪlɪti', 'ˈeɪb(ə)l', 'əˈbaʊt', 'əˈbʌv', 'əkˈsɛpt', 'əˈkɔːdɪŋ', 'əˈkaʊnt', 'əˈkrɒs', 'akt', 'ˈakʃ(ə)n', 'akˈtɪvɪti', 'ˈaktʃəli', 'ad', 'əˈdrɛs', 'ədmɪnɪˈstreɪʃ(ə)n', 'ədˈmɪt', 'ˈadʌlt', 'əˈfɛkt', 'ˈɑːftə', 'əˈɡɛn', 'əˈɡɛnst', 'eɪdʒ', 'ˈeɪdʒ(ə)nsi', 'ˈeɪdʒ(ə)nt', 'əˈɡəʊ', 'əˈɡriː', 'əˈɡriːm(ə)nt', 'əˈhɛd', 'ɛː', 'ɔːl', 'əˈlaʊ', 'ˈɔːlməʊst', 'əˈləʊn', 'əˈlɒŋ', 'ɔːlˈrɛdi', 'ˈɔːlsəʊ', 'ɔːlˈðəʊ', 'ˈɔːlweɪz', 'əˈmʌŋ', 'əˈmaʊnt', 'əˈnalɪsɪs', 'ənd', 'ˈanɪm(ə)l', 'əˈnʌðə', 'ˈɑːnsə', 'ˈɛni', 'ˈɛnɪwʌn', 'ˈɛnɪθɪŋ', 'əˈpɪə', 'əˈplʌɪ', 'əˈprəʊtʃ', 'ˈɛːrɪə', 'ˈɑːɡjuː', 'ɑːm', 'əˈraʊnd', 'əˈrʌɪv', 'ɑːt', 'ˈɑːtɪk(ə)l', 'ˈɑːtɪst', 'az', 'ɑːsk', 'əˈsjuːm', 'at', 'əˈtak', 'əˈtɛnʃ(ə)n', 'əˈtəːni', 'ˈɔːdɪəns', 'ˈɔːθə', 'ɔːˈθɒrɪti', 'əˈveɪləb(ə)l', 'əˈvɔɪd', 'əˈweɪ', 'ˈbeɪbi', 'bak', 'bad', 'baɡ', 'bɔːl', 'baŋk', 'bɑː', 'beɪs', 'biː', 'biːt', 'ˈbjuːtɪfʊl', 'bɪˈkɒz', 'bɪˈkʌm', 'bɛd', 'bɪˈfɔː', 'bɪˈɡɪn', 'bəˈhāvyər', 'bɪˈhʌɪnd', 'bɪˈliːv', 'ˈbɛnɪfɪt', 'bɛst', 'ˈbɛtə', 'bɪˈtwiːn', 'bɪˈjɒnd', 'bɪɡ', 'bɪl', 'ˈbɪljən', 'bɪt', 'blak', 'blʌd', 'bluː', 'bɔːd', 'ˈbɒdi', 'bʊk', 'bɔːn', 'bəʊθ', 'bɒks', 'bɔɪ', 'breɪk', 'brɪŋ', 'ˈbrʌðə', 'ˈbʌdʒɪt', 'bɪld', 'ˈbɪldɪŋ', 'ˈbɪznəs', 'bʌt', 'bʌɪ', 'bʌɪ', 'kɔːl', 'ˈkam(ə)rə', 'kamˈpeɪn', 'kan', 'ˈkansə', 'ˈkandɪdeɪt', 'ˈkapɪt(ə)l', 'kɑː', 'kɑːd', 'kɛː', 'kəˈrɪə', 'ˈkari', 'keɪs', 'katʃ', 'kɔːz', 'sɛl', 'ˈsen(t)ər', 'ˈsɛntr(ə)l', 'ˈsɛntʃʊri', 'ˈsəːt(ə)n', 'ˈsəːt(ə)nli']
