55824a5b33d5ba16dd0480d9e1072e2313d0ced8
Arnav <arnjain3@gmail.com>
2020-01-18 16:57:24 +0530

    random things changed, updated & deleted.

--- findNreplace.py
+++ findNreplace.py
@@ -1,34 +1,26 @@
-sentence = 'hate in the nation'
-sentenceSplit = ['hate', 'in', 'the', 'nation']
-pspelling = ['heɪt', 'ɪn', 'ðə', 'ˈneɪʃ(ə)n']
-numPspelling = ['wʌn', 'tuː', 'θriː', 'fɔː', 'fʌɪv',
-				'sɪks', 'ˈsɛv(ə)n', 'eɪt', 'nʌɪn', 'ˈzɪərəʊ']
-space = " "
-nospace = ""
-wordMatch = []
-#numspelling = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-#combined = {'1': 'wʌn', '2': 'tuː', '3': 'θriː', '4': 'fɔː', '5': 'fʌɪv','6': 'sɪks', '7': 'ˈsɛv(ə)n', '8': 'eɪt', '9': 'nʌɪn', '0': 'ˈzɪərəʊ'}
+# coding=utf-8
 
-#----------Logic-----------#
-i, j, a, b, c = 0, 0, 0, 0, 0
-for i in range(len(pspelling)):
-	singleWordno = nospace.join(pspelling[i]).split()
-	singleWord = space.join(pspelling[i]).split()
-	print("<      " + str(singleWord))
-	for j in range(len(numPspelling)):
-		singleNum = space.join(numPspelling[j]).split()
-		singleNumno = nospace.join(numPspelling[j]).split()
-		# print(singleNum)
-		wordMatch = []
-		for a in range(len(singleNum)):
-			for b in range(len(singleWord)):
-				if len(singleNum) <= len(singleWord):
-					if singleNum[a] == singleWord[b]:
-						wordMatch.extend(singleNum)
-						if len(wordMatch) >= 3:
-							for c in range(len(numPspelling)):
-								if "".join(str(wordMatch)) == "".join(str(numPspelling[c])):
-									print(wordMatch)
-								else:
-									pass
+numPspelling = ['wʌn', 'tuː', 'θriː', 'fɔː', 'fʌɪv',
+                'sɪks', 'ˈsɛv(ə)n', 'eɪt', 'nʌɪn', 'ˈzɪərəʊ']
+finalString = []
+def findNreplace(pspelling):
+    num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    for x in range(len(numPspelling)):
+        if __name__ == '__main__':
+            txt = " ".join(pspelling)
+            pat = numPspelling[x]
+            M = len(pat)
+            N = len(txt)
+            # A loop to slide pat[] one by one */
+            for i in range(N - M + 1):
+                j = 0
+                # For current index i, check
+                # for pattern match */
+                while(j < M):
+                    if (txt[i + j] != pat[j]):
+                        break
+                    j += 1
+
+                if (j == M):
+                    result = finalString[:i] + str(num[x]) + finalString[i+len(numPspelling[x]):]
+                    result result
