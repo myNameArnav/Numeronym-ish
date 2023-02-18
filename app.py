73645ea3db7229a045fb7decc0175bee7e5445ce
Arnav Jain <arnjain3@gmail.com>
2023-02-19 02:35:47 +0530

    removed commented code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from util import readJSON, findAndReplace
+from util import readJSON, findAndReplace, arrayAsString
 from ipa import numbersIPA, changeToIPA, transformSentence, ipaToWords
 from downloadJSON import downloadIPAJson
 from os.path import exists
@@ -12,15 +12,10 @@ def main():
     ipaNumbers = numbersIPA(ipaJSON)
     sentence = input("Enter a sentence: ")
     ipaSentence = changeToIPA(ipaJSON, sentence)
-    # print(ipaSentence)
     transformedSentence = transformSentence(ipaNumbers, ipaSentence)
-    # print(transformedSentence)
-    findAndReplace = findAndReplace(ipaSentence, transformedSentence)
-    # print(findAndReplace)
-    print((sentence, ipaSentence, findAndReplace))
-    finalSentence = ipaToWords(sentence, ipaSentence, findAndReplace)
-    print(finalSentence)
-    print(" ".join(x for x in finalSentence))
+    findReplace = findAndReplace(ipaSentence, transformedSentence)
+    finalSentence = ipaToWords(sentence, ipaSentence, findReplace)
+    arrayAsString(finalSentence)
 
 
 if __name__ == "__main__":
